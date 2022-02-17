f16463a0261b44a52691c2adc0ce23e3bdd60b6d
Raul Steurer <raul@steurer.io>
2022-02-17 12:56:50 +0100

    removed obsolete mock

--- __mocks__/react-markdown.tsx
+++
@@ -1,9 +0,0 @@
-interface ReactMarkdownProps {
-  children: string;
-}
-
-const ReactMarkdown: React.FC<ReactMarkdownProps> = ({
-  children,
-}: ReactMarkdownProps) => <div>{children}</div>;
-
-export default ReactMarkdown;
