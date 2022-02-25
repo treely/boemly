8dc546b7127eef45ba1ce0e6a9462fd528b0a14f
Lukas Bals <mail@lukasbals.at>
2022-02-25 14:52:12 +0100

    fix(RichText): Fix the RichText component to apply the textProps correct

--- src/components/RichText/RichText.tsx
+++ src/components/RichText/RichText.tsx
@@ -37,60 +37,63 @@ export const RichText: React.FC<RichTextProps> = ({
   headingProps = {},
   textProps = {},
 }: RichTextProps) => (
-  <Markdown
-    style={{ color: textProps.color }}
-    options={{
-      overrides: {
-        h1: ({ children }: ComponentProps) => (
-          <Heading as="h2" size="2xl" mb="6" mt="8" {...headingProps}>
-            {children}
-          </Heading>
-        ),
-        h2: ({ children }: ComponentProps) => (
-          <Heading as="h3" size="xl" mb="4" mt="6" {...headingProps}>
-            {children}
-          </Heading>
-        ),
-        h3: ({ children }: ComponentProps) => (
-          <Heading as="h4" size="lg" mb="4" mt="6" {...headingProps}>
-            {children}
-          </Heading>
-        ),
-        h4: ({ children }: ComponentProps) => (
-          <Heading as="h5" size="md" mb="4" mt="6" {...headingProps}>
-            {children}
-          </Heading>
-        ),
-        p: ({ children }: ComponentProps) => (
-          <Text size="mdRegularNormal" color="gray.800" mb="12" {...textProps}>
-            {children}
-          </Text>
-        ),
-        code: ({ children }: ComponentProps) => (
-          <Box backgroundColor="black" borderRadius="lg" padding="4" mb="12">
-            <Text size="mdMonoNormal" color="white" {...textProps}>
+  <Box {...textProps}>
+    <Markdown
+      options={{
+        overrides: {
+          h1: ({ children }: ComponentProps) => (
+            <Heading as="h2" size="2xl" mb="6" mt="8" {...headingProps}>
+              {children}
+            </Heading>
+          ),
+          h2: ({ children }: ComponentProps) => (
+            <Heading as="h3" size="xl" mb="4" mt="6" {...headingProps}>
+              {children}
+            </Heading>
+          ),
+          h3: ({ children }: ComponentProps) => (
+            <Heading as="h4" size="lg" mb="4" mt="6" {...headingProps}>
+              {children}
+            </Heading>
+          ),
+          h4: ({ children }: ComponentProps) => (
+            <Heading as="h5" size="md" mb="4" mt="6" {...headingProps}>
+              {children}
+            </Heading>
+          ),
+          p: ({ children }: ComponentProps) => (
+            <Text size="mdRegularNormal" color="gray.800" mb="12" {...textProps}>
               {children}
             </Text>
-          </Box>
-        ),
-        blockquote: ({ children }: ComponentProps) => (
-          <Box mb="12" pl="4" borderLeft="solid" borderLeftWidth="medium">
-            {children}
-          </Box>
-        ),
-        ul: ({ children }: ListComponentProps) => <BoemlyList listItems={getListItems(children)} />,
-        ol: ({ children }: ListComponentProps) => (
-          <BoemlyList listItems={getListItems(children)} ordered />
-        ),
-        a: ({ children, href }: LinkComponentProps) => (
-          <Link size="md" href={href}>
-            {children}
-          </Link>
-        ),
-        img: ({ src, alt }: ImageComponentProps) => <ImageContainer src={src} alt={alt} />,
-      },
-    }}
-  >
-    {content}
-  </Markdown>
+          ),
+          code: ({ children }: ComponentProps) => (
+            <Box backgroundColor="black" borderRadius="lg" padding="4" mb="12">
+              <Text size="mdMonoNormal" color="white" {...textProps}>
+                {children}
+              </Text>
+            </Box>
+          ),
+          blockquote: ({ children }: ComponentProps) => (
+            <Box mb="12" pl="4" borderLeft="solid" borderLeftWidth="medium">
+              {children}
+            </Box>
+          ),
+          ul: ({ children }: ListComponentProps) => (
+            <BoemlyList listItems={getListItems(children)} />
+          ),
+          ol: ({ children }: ListComponentProps) => (
+            <BoemlyList listItems={getListItems(children)} ordered />
+          ),
+          a: ({ children, href }: LinkComponentProps) => (
+            <Link size="md" href={href}>
+              {children}
+            </Link>
+          ),
+          img: ({ src, alt }: ImageComponentProps) => <ImageContainer src={src} alt={alt} />,
+        },
+      }}
+    >
+      {content}
+    </Markdown>
+  </Box>
 );
