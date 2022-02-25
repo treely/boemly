9f08a6de5633e70c133d4112a7508490869285c8
Lukas Bals <mail@lukasbals.at>
2022-02-25 09:51:26 +0100

    Document breakpoints

---
+++ stories/tokens/Breakpoints.stories.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Meta } from '@storybook/react';
+
+import { Table, Tbody, Td, Text, Th, Thead, Tr, useToken } from '@chakra-ui/react';
+import { BREAKPOINTS } from '../../src/constants/customizations';
+
+export default {
+  title: 'Tokens/Breakpoints',
+} as Meta;
+
+interface BreakpointRowProps {
+  breakpoint: string;
+}
+
+const BreakpointRow = ({ breakpoint }: BreakpointRowProps) => {
+  const [breakpointValue] = useToken('breakpoints', [breakpoint]);
+
+  return (
+    <Tr>
+      <Td>
+        <Text size="mdMonoNormal">{breakpoint}</Text>
+      </Td>
+      <Td>
+        <Text size="mdMonoNormal">{breakpointValue}</Text>
+      </Td>
+    </Tr>
+  );
+};
+
+export const Breakpoints = () => (
+  <Table variant="simple">
+    <Thead>
+      <Tr>
+        <Th>Token</Th>
+        <Th>Value</Th>
+      </Tr>
+    </Thead>
+    <Tbody>
+      {Object.keys(BREAKPOINTS).map((breakpoint) => (
+        <BreakpointRow key={breakpoint} breakpoint={breakpoint} />
+      ))}
+    </Tbody>
+  </Table>
+);
