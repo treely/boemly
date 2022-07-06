d1157b84da53e6e8fe6bf98c3bb0da5a64092338
Lukas Bals <mail@lukasbals.at>
2022-07-06 14:22:08 +0200

    feat(SplitScreen): Clean up unneeded CSS

--- src/components/SplitScreen/styles.ts
+++ src/components/SplitScreen/styles.ts
@@ -37,20 +37,5 @@ export const Right = styled(motion.div)<RightProps>`
       hideLeftOnMobile ? '0' : 'var(--boemly-radii-2xl)'};
     box-shadow: ${({ hideLeftOnMobile }) =>
       hideLeftOnMobile ? 'none' : 'var(--boemly-shadows-md)'};
-
-    // This is just needed if the right container contains a mapbox map.
-    // Since it's no problem if there are other components in the right
-    // container I think it's ok to keep that here.
-    & .mapboxgl-map {
-      overflow: hidden;
-      border-top-right-radius: ${({ hideLeftOnMobile }) =>
-        hideLeftOnMobile ? '0' : 'var(--boemly-radii-2xl)'};
-      border-top-left-radius: ${({ hideLeftOnMobile }) =>
-        hideLeftOnMobile ? '0' : 'var(--boemly-radii-2xl)'};
-      box-shadow: ${({ hideLeftOnMobile }) =>
-        hideLeftOnMobile ? 'none' : 'var(--boemly-shadows-md)'};
-
-      mask-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAA5JREFUeNpiYGBgAAgwAAAEAAGbA+oJAAAAAElFTkSuQmCC);
-    }
   }
 `;
