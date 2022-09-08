b66e6bb8658bc831ea67036ad232c2937343dffb
Raul Steurer <raul@steurer.io>
2022-09-08 14:37:52 +0200

    perf(Container): Do not apply 'styleProps' to parent of children
    BREAKING CHANGE: There now is an additional Box
    between the Container component and its children.
    If you are using the 'styleProps' on the Container to
    customize the layout of its children, it is advised to
    pass the children inside of a single Box, Flex or Grid
    component and to use that component to customize
    the layout of the children.

--- src/components/ContactArea/ContactArea.tsx
+++ src/components/ContactArea/ContactArea.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Divider, Heading, Text } from '@chakra-ui/react';
+import { Box, Button, Divider, Heading, Text } from '@chakra-ui/react';
 import { ArrowRight } from 'phosphor-react';
 import { AvatarWithName, AvatarWithNameProps } from '../AvatarWithName/AvatarWithName';
 import { Container } from '../Container';
@@ -26,22 +26,25 @@ export const ContactArea: React.FC<ContactAreaProps> = ({
       {text}
     </Text>
 
-    <Container
-      display="flex"
-      flexDir={['column', null, null, 'row']}
-      alignItems={['flex-start', null, null, 'center']}
-      justifyContent="space-between"
-    >
-      <AvatarWithName name={avatar.name} description={avatar.description} image={avatar.image} />
-      <Button
-        mt={['6', null, null, '0']}
-        colorScheme="whiteAlpha"
-        variant="outline"
-        rightIcon={<ArrowRight />}
-        onClick={link.onClick}
+    <Container>
+      <Box
+        display="flex"
+        height="full"
+        flexDir={['column', null, null, 'row']}
+        alignItems={['flex-start', null, null, 'center']}
+        justifyContent="space-between"
       >
-        {link.text}
-      </Button>
+        <AvatarWithName name={avatar.name} description={avatar.description} image={avatar.image} />
+        <Button
+          mt={['6', null, null, '0']}
+          colorScheme="whiteAlpha"
+          variant="outline"
+          rightIcon={<ArrowRight />}
+          onClick={link.onClick}
+        >
+          {link.text}
+        </Button>
+      </Box>
     </Container>
   </>
 );
