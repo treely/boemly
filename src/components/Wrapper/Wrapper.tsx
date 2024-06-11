9e03046fbe47be38dbd1d83eb42894d8073cdf67
lialila <uche0790@gmail.com>
2024-06-11 12:02:46 +0200

    feat(Header): Add conditional Wrapper

--- src/components/Wrapper/Wrapper.tsx
+++ src/components/Wrapper/Wrapper.tsx
@@ -5,7 +5,13 @@ interface WrapperProps {
   children: JSX.Element | JSX.Element[];
 }
 export const Wrapper: React.FC<WrapperProps> = ({ children }: WrapperProps) => (
-  <Box maxWidth="7xl" marginX="auto" paddingX={['6', null, '8']} position="relative">
+  <Box
+    maxWidth="7xl"
+    marginX="auto"
+    paddingX={['6', null, '8']}
+    position="relative"
+    data-testid="wrapper"
+  >
     {children}
   </Box>
 );
