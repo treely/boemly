45184b490aa2a3e93c70e3cf8242f0e5809aa469
ClaraTschamon <clara.tschamon@tree.ly>
2024-09-10 09:43:55 +0200

    feat(slider): add boundary variant

--- src/components/Slider/Slider.test.tsx
+++ src/components/Slider/Slider.test.tsx
@@ -31,64 +31,96 @@ describe('The Slider component', () => {
     onChangeSpy.mockRestore();
   });
 
-  it('displays a slider', () => {
-    setup();
+  describe('in the default variant', () => {
+    it('displays a slider', () => {
+      setup();
 
-    expect(screen.getByRole('slider')).toBeInTheDocument();
+      expect(screen.getByRole('slider')).toBeInTheDocument();
+    });
+
+    it('displays an input field', () => {
+      setup();
+
+      expect(screen.getByRole('spinbutton')).toBeInTheDocument();
+    });
+
+    it('calls the onChange function if the input is used', () => {
+      setup();
+
+      fireEvent.input(screen.getByRole('spinbutton'), { target: { value: '12' } });
+
+      expect(onChangeSpy).toHaveBeenCalledWith(12);
+    });
+
+    it('does not call the onChange function if the input value is an empty string', () => {
+      setup();
+
+      fireEvent.input(screen.getByRole('spinbutton'), { target: { value: '' } });
+
+      expect(onChangeSpy).toHaveBeenCalledTimes(0);
+    });
+
+    it('calls the onChange function with the minimum if the value in the input field is lower then minimum', () => {
+      setup();
+
+      fireEvent.input(screen.getByRole('spinbutton'), { target: { value: '-1' } });
+
+      expect(onChangeSpy).toHaveBeenCalledWith(0);
+    });
+
+    it('calls the onChange function with the maximum if the value in the input field is higher then maximum', () => {
+      setup();
+
+      fireEvent.input(screen.getByRole('spinbutton'), { target: { value: '101' } });
+
+      expect(onChangeSpy).toHaveBeenCalledWith(100);
+    });
+
+    // We can't test if the slider triggers the onChange function since we have to polyfill the ResizeObserver
+
+    it('displays the unit if given', () => {
+      setup({ unit: 'unit' });
+
+      expect(screen.getByText('unit')).toBeInTheDocument();
+      expect(screen.getByText('0 unit')).toBeInTheDocument();
+      expect(screen.getByText('100 unit')).toBeInTheDocument();
+    });
+
+    it('displays custom min and max values', () => {
+      setup({ min: 1, max: 99 });
+
+      expect(screen.getByText('1')).toBeInTheDocument();
+      expect(screen.getByText('99')).toBeInTheDocument();
+    });
   });
 
-  it('displays an input field', () => {
-    setup();
+  describe('in the boundary variant', () => {
+    it('displays a boundary slider', () => {
+      setup({ variant: 'boundary' });
 
-    expect(screen.getByRole('spinbutton')).toBeInTheDocument();
-  });
+      expect(screen.getAllByRole('slider')).toHaveLength(3);
+    });
 
-  it('calls the onChange function if the input is used', () => {
-    setup();
+    it('displays the correct boundary slider min and max texts', () => {
+      setup({
+        variant: 'boundary',
+        min: 10,
+        max: 90,
+        unit: 'kg',
+        lowerBoundText: 'Low',
+        upperBoundText: 'High',
+      });
 
-    fireEvent.input(screen.getByRole('spinbutton'), { target: { value: '12' } });
+      expect(screen.getByText('Low')).toBeInTheDocument();
+      expect(screen.getByText('High')).toBeInTheDocument();
+      expect(screen.getByText('10 kg')).toBeInTheDocument();
+      expect(screen.getByText('90 kg')).toBeInTheDocument();
+    });
 
-    expect(onChangeSpy).toHaveBeenCalledWith(12);
-  });
+    it('displays a tooltip with the correct value', () => {
+      setup({ variant: 'boundary' });
 
-  it('does not call the onChange function if the input value is an empty string', () => {
-    setup();
-
-    fireEvent.input(screen.getByRole('spinbutton'), { target: { value: '' } });
-
-    expect(onChangeSpy).toHaveBeenCalledTimes(0);
-  });
-
-  it('calls the onChange function with the minimum if the value in the input field is lower then minimum', () => {
-    setup();
-
-    fireEvent.input(screen.getByRole('spinbutton'), { target: { value: '-1' } });
-
-    expect(onChangeSpy).toHaveBeenCalledWith(0);
-  });
-
-  it('calls the onChange function with the maximum if the value in the input field is higher then maximum', () => {
-    setup();
-
-    fireEvent.input(screen.getByRole('spinbutton'), { target: { value: '101' } });
-
-    expect(onChangeSpy).toHaveBeenCalledWith(100);
-  });
-
-  // We can't test if the slider triggers the onChange function since we have to polyfill the ResizeObserver
-
-  it('displays the unit if given', () => {
-    setup({ unit: 'unit' });
-
-    expect(screen.getByText('unit')).toBeInTheDocument();
-    expect(screen.getByText('0 unit')).toBeInTheDocument();
-    expect(screen.getByText('100 unit')).toBeInTheDocument();
-  });
-
-  it('displays custom min and max values', () => {
-    setup({ min: 1, max: 99 });
-
-    expect(screen.getByText('1')).toBeInTheDocument();
-    expect(screen.getByText('99')).toBeInTheDocument();
+      expect(screen.getByRole('tooltip')).toBeInTheDocument();
+    });
   });
 });
