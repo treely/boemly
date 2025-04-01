72e1a1a1f10625fe22fe590c7a018935528e5d1b
rschiffer <roman.schiffer@gmail.com>
2025-04-01 13:03:57 +0200

    fix: boemly steps

--- src/components/BoemlySteps/StepDivider.tsx
+++ src/components/BoemlySteps/StepDivider.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import { Box } from '@chakra-ui/react';
+import { Box, Separator } from '@chakra-ui/react';
 
 const StepDivider = () => (
   <Box position="relative" width="8" marginX="4">
-    <Box
+    <Separator
       position="absolute"
       top="50%"
       width="full"
-      borderTop="1px"
       borderColor="blackAlpha.400"
       data-testid="step-divider"
     />
